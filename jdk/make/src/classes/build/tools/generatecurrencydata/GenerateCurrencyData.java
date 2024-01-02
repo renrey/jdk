0b8ab02b31aade84aebee8fde61d9240455d4713
renrey <renamiruki@gmail.com>
2024-01-02 20:04:47 +0800

    Update GenerateCurrencyData.java：屏蔽java代码时间验证导致编译出错

--- jdk/make/src/classes/build/tools/generatecurrencydata/GenerateCurrencyData.java
+++ jdk/make/src/classes/build/tools/generatecurrencydata/GenerateCurrencyData.java
@@ -281,9 +281,9 @@ public class GenerateCurrencyData {
             checkCurrencyCode(newCurrency);
             String timeString = currencyInfo.substring(4, length - 4);
             long time = format.parse(timeString).getTime();
-            if (Math.abs(time - System.currentTimeMillis()) > ((long) 10) * 365 * 24 * 60 * 60 * 1000) {
-                throw new RuntimeException("time is more than 10 years from present: " + time);
-            }
+            //if (Math.abs(time - System.currentTimeMillis()) > ((long) 10) * 365 * 24 * 60 * 60 * 1000) {
+            //    throw new RuntimeException("time is more than 10 years from present: " + time);
+            //}
             specialCaseCutOverTimes[specialCaseCount] = time;
             specialCaseOldCurrencies[specialCaseCount] = oldCurrency;
             specialCaseOldCurrenciesDefaultFractionDigits[specialCaseCount] = getDefaultFractionDigits(oldCurrency);
